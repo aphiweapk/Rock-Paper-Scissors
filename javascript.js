1c2b02e43367b2e4493b8dbdbdef7992ee700446
Aphiwe Khumalo <Aphiweayanda74@gmail.com>
2021-06-27 16:08:03 +0200

    An updated version of the rock paper scissors game that has a user interface

--- javascript.js
+++ javascript.js
@@ -1,111 +1,152 @@
-// console.log('its connected!')
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+const compScore = document.querySelector('.computer-score');
+const playerScore = document.querySelector('.player-score');
+
+const button = document.querySelectorAll('.disabled'); // play buttons
+const winPlaceholder = document.querySelector('.winner'); 
+const restart = document.querySelector('.restart'); //restart button
+
+restart.addEventListener('click', () => {
+    compScore.textContent = 0;
+    playerScore.textContent = 0;
+
+    button.forEach(button => {
+        button.disabled = false;
+        location.reload();
+    });
+})
+
+
+rock.addEventListener('click', () => {
+    let ans = playRound('rock', computerPlay());
+    compScore.textContent = ans.comp;
+    playerScore.textContent = ans.player;
+
+    button.forEach(button => {
+        if(ans.comp === 5 || ans.player === 5){
+            button.disabled = true;
+        }
+    });
+
+    if(ans.comp === 5){
+            winPlaceholder.textContent = 'Computer Wins'
+        }else if (ans.player === 5){
+            winPlaceholder.textContent = 'Player Wins'
+        }
+
+})
+
+paper.addEventListener('click', () => {
+    let ans = playRound('paper', computerPlay());
+    compScore.textContent = ans.comp;
+    playerScore.textContent = ans.player;
+
+    button.forEach(button => {
+        if(ans.comp === 5 || ans.player === 5){
+        button.disabled = true;
+        }
+    });
+
+    if(ans.comp === 5){
+            winPlaceholder.textContent = 'Computer Wins'
+        }else if (ans.player === 5){
+            winPlaceholder.textContent = 'Player Wins'
+        }
+
+})
+
+scissors.addEventListener('click', () => {
+    let ans = playRound('scissors', computerPlay());
+    compScore.textContent = ans.comp;
+    playerScore.textContent = ans.player;
+
+    button.forEach(button => {
+        if(ans.comp === 5 || ans.player === 5){
+        button.disabled = true;
+        }
+    });
+
+    if(ans.comp === 5){
+            winPlaceholder.textContent = 'Computer Wins'
+        }else if (ans.player === 5){
+            winPlaceholder.textContent = 'Player Wins'
+        }
+
+})
+
+
 
 function computerPlay(){
 
-    let choices = ['Rock', 'Paper', 'Scissors']
-
     let randomIndex = Math.floor(Math.random() * 3);
-    // console.log(randomIndex)
-    return choices[randomIndex];
+    return randomIndex;
 
 }
+
+let scores = {
+    comp: 0,
+    player: 0
+}
+
 
 function playRound(playerSelection, computerSelection){
 
     let choices = ['rock', 'paper', 'scissors'];
+            // player chooses  ROCK here
 
-    let scores = {
-        comp: 0,
-        player: 0
-    }
-
-    // player chooses  ROCK here
-    if(playerSelection.toLowerCase() === choices[0]){
-        if(computerSelection.toLowerCase() === choices[2]){
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        if(playerSelection.toLowerCase() === choices[0]){
+        if(choices[computerSelection] === choices[2]){
+            alert(`You win! ${playerSelection} beats ${choices[computerSelection]}`)
             scores.player ++;
-        }else if(computerSelection.toLowerCase() === choices[1]){
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        }else if(choices[computerSelection] === choices[1]){
+            alert(`You lose! ${choices[computerSelection]} beats ${playerSelection}`)
             scores.comp ++;
         }else{
-            console.log('It is a draw')
+            alert('It is a draw')
         }
     }
 
     // player chooses  PAPER here
     if(playerSelection.toLowerCase() === choices[1]){
-        if(computerSelection.toLowerCase() === choices[0]){
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        if(choices[computerSelection] === choices[0]){
+            alert(`You win! ${playerSelection} beats ${choices[computerSelection]}`)
             scores.player ++;
-        }else if(computerSelection.toLowerCase() === choices[2]){
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        }else if(choices[computerSelection] === choices[2]){
+           alert(`You lose! ${choices[computerSelection]} beats ${playerSelection}`)
             scores.comp ++;
         }else{
-            console.log('It is a draw')
+            alert('It is a draw')
         }
     }
 
     // player chooses scissors
 
     if(playerSelection.toLowerCase() === choices[2]){
-        if(computerSelection.toLowerCase() === choices[1]){
-            console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+        if(choices[computerSelection] === choices[1]){
+            alert(`You win! ${playerSelection} beats ${choices[computerSelection]}`)
             scores.player ++;
-        }else if(computerSelection.toLowerCase() === choices[0]){
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
+        }else if(choices[computerSelection] === choices[0]){
+            alert(`You lose! ${choices[computerSelection]} beats ${playerSelection}`)
             scores.comp ++;
         }else{
-            console.log('It is a draw')
+            alert('It is a draw')
         }
-    }
-
+        }
     return scores;
 
 }
 
-function game(){
-
-    let play = 0;
-
-    let compScore = 0;
-    let playerScore = 0;
-
-    while(play < 5){
-
-        play ++;
-
-        let player = prompt('Rock, Paper or Scissors?');
-
-        const computer = computerPlay();
-        
-        let increment = playRound(player, computer)
-        compScore += increment.comp
-        playerScore += increment.player
-
-    }
-
-    let finalScore = ''
-
-    if(compScore > playerScore){
-        finalScore = `Final Score: 
-Computer: ${compScore}
-Player: ${playerScore}
-        
-        Computer Wins`
-    }else if(compScore < playerScore){
-        finalScore = `Final Score: 
-Player: ${playerScore}
-Computer: ${compScore}
-        
-        Player Wins`
-    }else{
-        finalScore = `Draw 
-Computer: ${compScore}
-Player: ${playerScore}`
-    }
-
-    return finalScore;
+function display(){
+    let clock = document.querySelector('.time');
+    let now = new Date();
+    clock.textContent = now.toLocaleTimeString()
 }
 
+display()
 
-console.log(game())
+setInterval(display, 1000)
+
+
